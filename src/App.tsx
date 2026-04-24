75e5fb74490d94db4074552ead90f76958fbdbf7
Ayushmaan Mishra <ayush8171wiz@gmail.cpm>
2026-04-24 20:11:39 +0530

    secure routes made

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
 import "./App.css";
 import AllRoutes from "./routes/AllRoutes";
-import { AuthProvider } from "./context/AuthContext";
-import { useEffect } from "react";
-import { socket } from "./utils/socket";
 
 function App() {
-  const userId = localStorage.getItem("userId");
-
-  useEffect(() => {
-    if (userId) {
-      socket.emit("register", userId);
-    }
-  }, [userId]);
-  return (
-    <AuthProvider>
-      <AllRoutes />
-    </AuthProvider>
-  );
+  return <AllRoutes />;
 }
 
 export default App;
