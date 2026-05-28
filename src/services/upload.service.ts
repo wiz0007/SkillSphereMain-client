815c19efe9feacfdd35d1b875a408a55df46303d
Ayushmaan Mishra <ayush99wiz@gmail.com>
2026-05-28 19:04:10 +0530

    updates

--- src/services/upload.service.ts
+++ src/services/upload.service.ts
@@ -4,11 +4,7 @@ export const uploadProfilePhoto = async (file: File): Promise<string> => {
   const formData = new FormData();
   formData.append("profilePhoto", file);
 
-  const res = await api.post("/profile/upload-photo", formData, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
+  const res = await api.post("/profile/upload-photo", formData);
 
   return res.data.imageUrl;
 };
